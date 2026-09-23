4b131e2c04cad67b326eeda646a8f9fbb9943b54
Tim Kightly <tim@tkightly.uk>
2026-09-23 21:17:24 +0000

    feat: add ListBooks() function

--- sqlite/sqlite.go
+++ sqlite/sqlite.go
@@ -36,7 +36,34 @@ func NewDb(path string) (*Db, error) {
 
 func (d *Db) ListBooks(ctx context.Context) ([]book.Book, error) {
 
-	books := []book.Book{}
+	query := "SELECT ObjectID, Title, Author, ImageURL FROM books"
+
+	rows, err := d.conn.QueryContext(ctx, query)
+
+	if err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
+
+	defer rows.Close()
+
+	var books []book.Book
+
+	for rows.Next() {
+
+		var book book.Book
+
+		if err := rows.Scan(&book.ObjectID, &book.Title, &book.Author, &book.ImageURL); err != nil {
+			return nil, fmt.Errorf("%w", err)
+
+		}
+
+		books = append(books, book)
+
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("%w", err)
+	}
 
 	return books, nil
 }
